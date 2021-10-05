0959febec2f7eb30d44163d04fb21833f2897f4a
Giorgio Andrei <cipuandrei@libero.it>
2021-10-05 15:03:31 +0200

    construct added

--- index.php
+++ index.php
@@ -1,11 +1,25 @@
 <?php
 class Movie
 {
-
     public $title;
     public $duration;
+    public $director;
     public $actors;
     public $description;
     public $ranking;
     public $yearOfRelease;
+
+
+
+    public  function  __construct($_title, $_duration, $_actors, $_description, $_ranking, $_yearOfRelease, $_director = 'Anonymous')
+    {
+
+        $this->title = $_title;
+        $this->duration = $_duration;
+        $this->actors = $_actors;
+        $this->description = $_description;
+        $this->ranking = $_ranking;
+        $this->yearOfRelease = $_yearOfRelease;
+        $this->director = $_director;
+    }
 };
